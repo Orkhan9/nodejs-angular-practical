078592b6f2321081079db465cfa5f9b20cac24e4
Orkhan Mammadli <orkhanmm@code.edu.az>
2021-02-26 00:01:16 +0400

    fetching from database

--- backend/app.js
+++ backend/app.js
@@ -38,16 +38,14 @@ app.post('/api/posts',(req,res,next)=>{
 })
 
 app.get('/api/posts',(req,res,next)=>{
-    const posts=[
-        {id:'1',title:'1st title from server',content:'1st content from server'},
-        {id:'2',title:'2nd title from server',content:'2nd content from server'},
-        {id:'3',title:'3rd title from server',content:'3rd content from server'},
-        {id:'4',title:'4th title from server',content:'4th content from server'},
-    ]
-    res.status(200).json({
-        message:'Posts fetched successfully',
-        posts:posts
-    });
-})
+    Post.find()
+        .then(documents=>{
+            console.log(documents)
+            res.status(200).json({
+                message:'Posts fetched successfully',
+                posts:documents
+            });
+        })
+    })
 
 module.exports=app;
