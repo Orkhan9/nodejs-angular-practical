8eceae25771e1c355c02fd06d9f8e7ab12776714
Orkhan Mammadli <orkhanmm@code.edu.az>
2021-03-05 12:39:50 +0400

    connecting front paginator to the backend

--- frontend/src/app/posts/post-list/post-list.component.ts
+++ frontend/src/app/posts/post-list/post-list.component.ts
@@ -14,13 +14,14 @@ export class PostListComponent implements OnInit,OnDestroy {
   isLoading:boolean=false;
   totalPosts=10;
   postsPerPage=1;
+  currentPage=1;
   pageSizeOptions=[1,2,5,10];
   private postsSub=new Subscription();
   constructor(private postsService:PostsService) { }
 
   ngOnInit(): void {
     this.isLoading=true;
-    this.postsService.getPosts();
+    this.postsService.getPosts(this.postsPerPage,this.currentPage);
     this.postsSub=this.postsService.getPostsUpdatedListener()
       .subscribe((posts:Post[])=>{
         this.isLoading=false;
@@ -29,7 +30,9 @@ export class PostListComponent implements OnInit,OnDestroy {
   }
 
   onChangedPage(pageData:PageEvent){
-    console.log(pageData)
+    this.currentPage=pageData.pageIndex+1;
+    this.postsPerPage=pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage,this.currentPage);
   }
 
   onDelete(postId:string){
